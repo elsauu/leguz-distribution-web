19fd9c6e97dc063b0cf2f558e95e826b852b5395
elsauu <esau.garcia@uttn.mx>
2026-05-13 16:32:41 -0500

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LEGUZ.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
